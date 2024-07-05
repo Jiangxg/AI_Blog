fc6efac0acfd3797c27233be05729d2da33fbb85
Jiangxg <1578172112@qq.com>
2024-07-05 23:30:45 +0800

    testing

--- source/conf.py
+++ source/conf.py
@@ -11,22 +11,46 @@ copyright = '2024, Jiangxg'
 author = 'Jiangxg'
 version = '1.0'
 release = '1.0'
+    
+extensions = [
+    'myst_parser',
+    "sphinxcontrib.mermaid",
+    "sphinx_copybutton",
+]
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
-extensions = []
+myst_enable_extensions = [
+    "tasklist",
+    "deflist",
+    "dollarmath",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
 language = 'zh_CN'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'analytics_anonymize_ip': False,
+    'logo_only': True,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False,
+}
 
-#html_theme = 'alabaster'
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'classic'
-
+html_logo = "./_static/logo.png"
 html_static_path = ['_static']
+html_js_files = [
+    'my_custom.js',
+]
